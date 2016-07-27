1fba47e1cf1c00ab3f54189dd1f64d9cdf26049d
jkachurek <john.kachurek@gmail.com>
2016-07-27 11:01:58 -0400

    Added Google Analytics

---
+++ scripts/app/app-randomcolor.js
@@ -0,0 +1,6 @@
+var titleColor = randomColor({luminosity: 'dark'});
+$('#nameTitle').css({"color":titleColor});
+$('#subtitle').css({"color":titleColor});
+var buttonColor = randomColor({luminosity: 'dark'});
+$('#resumeButton').css({"background-color":buttonColor,"border-color":buttonColor,
+                        "color":randomColor({luminosity: 'light'})})
