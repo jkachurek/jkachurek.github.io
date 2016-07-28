15d16692a235e6f8a01ed23bc0cbf0d643a42d4d
jkachurek <john.kachurek@gmail.com>
2016-07-28 12:33:40 -0400

    restructuring with bootstrap

--- scripts/app/app-randomcolor.js
+++ scripts/app/app-randomcolor.js
@@ -2,5 +2,5 @@ var titleColor = randomColor({luminosity: 'dark'});
 $('#nameTitle').css({"color":titleColor});
 $('#subtitle').css({"color":titleColor});
 var buttonColor = randomColor({luminosity: 'dark'});
-$('#resumeButton').css({"background-color":buttonColor,"border-color":buttonColor,
+$('.rcolor').css({"background-color":buttonColor,"border-color":buttonColor,
                         "color":randomColor({luminosity: 'light'})})
