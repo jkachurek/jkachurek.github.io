03a4c998a1079b1ca14e54a1575d002aaa25010a
jkachurek <john.kachurek@gmail.com>
2016-07-28 13:38:55 -0400

    adding another project

--- scripts/app/screenshots.js
+++ scripts/app/screenshots.js
@@ -18,38 +18,6 @@ var screenshotSettings = {
 
 $(document).ready(function() {
   $('.xFidScreenshots').magnificPopup(screenshotSettings);
-  $('.battleshipScreenshots').magnificPopup({
-    delegate: 'a',
-    type: 'image',
-    tLoading: 'Loading image #%curr%...',
-    mainClass: 'mfp-img-mobile',
-    gallery: {
-      enabled: true,
-      navigateByImgClick: true,
-      preload: [0,1]
-    },
-    image: {
-      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-      titleSrc: function(item) {
-        return item.el.attr('title');
-      }
-    }
-  });
-  $('.HRPortalScreenshots').magnificPopup({
-    delegate: 'a',
-    type: 'image',
-    tLoading: 'Loading image #%curr%...',
-    mainClass: 'mfp-img-mobile',
-    gallery: {
-      enabled: true,
-      navigateByImgClick: true,
-      preload: [0,1]
-    },
-    image: {
-      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-      titleSrc: function(item) {
-        return item.el.attr('title');
-      }
-    }
-  });
+  $('.battleshipScreenshots').magnificPopup(screenshotSettings);
+  $('.HRPortalScreenshots').magnificPopup(screenshotSettings);
 });
