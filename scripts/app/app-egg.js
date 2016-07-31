90e19a9ab1577d7aaf0d32a5360f5df3542c5a19
jkachurek <john.kachurek@gmail.com>
2016-07-31 01:42:26 -0400

    trying to add an egg

--- scripts/app/app-egg.js
+++ scripts/app/app-egg.js
@@ -1,3 +1,3 @@
 var egg = new Egg("d,u,c,k", function(){
-  $('body').appendTo('<audio><source src=\"\\Content\\duck.mp3\" type=\"audio/mpeg\"></audio>')
+  $('body').appendTo('<audio><source src="/Content/duck.mp3" type=\"audio/mpeg\"></audio>')
 })
