3b5f86f8c9f150092322f8cdbc3ecb024b3900f9
jkachurek <john.kachurek@gmail.com>
2016-07-31 11:54:04 -0400

    testing egg

--- scripts/app/app-egg.js
+++ scripts/app/app-egg.js
@@ -1,5 +1,4 @@
 var egg = new Egg();
-egg
-  .addCode("d,u,c,k", function(){
-    $('body').append('<audio autoplay="true"><source src="/Content/duck.mp3" type=\"audio/mpeg\"></audio>')
-  });
+egg.addCode("d,u,c,k", function(){
+    $('#duck').attr('autoplay', 'true');
+});
