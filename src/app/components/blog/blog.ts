8b590004d94d1ef3da04a7aba1a2ee8748763f8c
John Kachurek <John.Kachurek@amtrustgroup.com>
2017-04-12 09:22:18 -0400

    Added pagination feature to blog page
    Buttons hidden when there are less than 6 posts.  But it will be useful in the future.

--- src/app/components/blog/blog.ts
+++ src/app/components/blog/blog.ts
@@ -12,11 +12,23 @@ BlogController.$inject = ['$stateParams'];
 function BlogController($stateParams: angular.ui.IStateParamsService) {
 	const ctrl = this;
 
+	const blogsPerPage: number = 5;
+	ctrl.pages = [];
+
 	ctrl.$onInit = () => {
 		ctrl.currentTag = $stateParams['tag'];
 		ctrl.tags = BlogRepo.getTagData();
 		ctrl.blogPosts = $stateParams['tag']
 			? (BlogRepo.getByExpression(b => ~b.tags.indexOf($stateParams['tag'])) || BlogRepo.getAll())
 			: BlogRepo.getAll();
+		for (let i = 0; i < Math.ceil(ctrl.blogPosts.length / blogsPerPage); i++) {
+			ctrl.pages.push(ctrl.blogPosts.slice(i * blogsPerPage, (i * blogsPerPage) + blogsPerPage));
+		}
+		ctrl.goToPage(0);
+	};
+	ctrl.goToPage = (pageNumber: number): void => {
+		if (!ctrl.pages[pageNumber]) return;
+		ctrl.currentPageNumber = pageNumber;
+		ctrl.currentPage = ctrl.pages[pageNumber];
 	};
 }
